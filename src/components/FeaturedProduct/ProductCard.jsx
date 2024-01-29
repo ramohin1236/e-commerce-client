65b8a92d44e557c874272aef9f7ba63faf3bd5d6
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-01-29 06:56:39 -0800

    store card

--- src/components/FeaturedProduct/ProductCard.jsx
+++ src/components/FeaturedProduct/ProductCard.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+/* eslint-disable react/prop-types */
+import { Link} from 'react-router-dom';
 import './ProductCard.css';
 import { CiHeart } from "react-icons/ci";
 import { FaBagShopping, FaEye } from "react-icons/fa6";
@@ -6,17 +7,22 @@ import ReactStars from "react-rating-stars-component";
 import { IoIosGitCompare } from "react-icons/io";
 
 const ProductCard = () => {
-   
+//     const {grid}=props
+//     console.log(grid);
+//    let location = useLocation()
     return (
-        <div className='col-3'>
+      
+         <div className='col-3'>
+         {/* className={`${location.pathname == '/store'?`gr-${grid}`: "col-3"}`}
+          */}
            <Link className='product-card position-relative'>
-            
+           <div className="card-product" >
            <div className="card-image">
-                    <img className="img-fluid" src="/public/cutg.png" alt="" />
-                    <img className="img-fluid" src="/public/cutg2.jpg" alt="" />
+                    <img className="img-fluid" src="/public/watch-1.png" alt="" />
+                    <img className="img-fluid" src="/public/f.png" alt="" />
                     
                 </div>
-                <div className="product-details">
+  <div className="product-details">
                     <h6 className='brand'>Google-Pixel </h6>
                     <h5>Google-pixel 7 pro</h5>
                     <ReactStars
@@ -36,8 +42,37 @@ const ProductCard = () => {
                    <Link><IoIosGitCompare className="fs-5 icon"/></Link>
                  </div>
                 </div>
+</div>
+            
+           {/* <div className="card-image">
+                    <img className="img-fluid" src="/public/cutg.png" alt="" />
+                    <img className="img-fluid" src="/public/cutg2.jpg" alt="" />
+                    
+                </div> */}
+                {/* <div className="product-details">
+                    <h6 className='brand'>Google-Pixel </h6>
+                    <h5>Google-pixel 7 pro</h5>
+                    <ReactStars
+    count={5}
+    size={24}
+    value={4}
+    edit={false}
+    activeColor="#ffd700"
+  />
+                    <p className='price'>$100.00</p>
+                </div> */}
+                {/* <div className="action-bar position-absolute">
+                 <div className='d-flex flex-column gap-15'>
+                 <Link><CiHeart  className="fs-5 icon"/> </Link>
+                   <Link><FaBagShopping  className="fs-5 icon"/></Link>
+                   <Link><FaEye className="fs-5 icon"/></Link>
+                   <Link><IoIosGitCompare className="fs-5 icon"/></Link>
+                 </div>
+                </div> */}
            </Link>
      </div>
+
+     
     );
 };
 
--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -5,9 +5,16 @@ import Meta from "../../components/Meta/Meta";
 import './Store.css';
 import ReactStars from "react-rating-stars-component";
 import { FaAlignJustify, FaBars, FaEquals } from "react-icons/fa";
+// import { useState } from "react";
+import ProductCard from './../../components/FeaturedProduct/ProductCard';
+
 
 
 const Store = () => {
+
+//     const [grid, setGrid] = useState(4)
+// console.log(grid);
+//     // alert(grid)
     return (
         <div>
             <Meta title={"Our Store"} />
@@ -51,30 +58,30 @@ const Store = () => {
                                                 Out Of Stock {0}
                                             </label>
                                         </div>
-                                       
+
                                     </div>
                                     {/* price */}
                                     <h5 className="sub-title mt-3">
                                         Price
                                     </h5>
                                     <div className="d-flex align-items-center gap-10">
-                                    <div className="form-floating ">
-  <input type="email" className="form-control" id="floatingInput1" placeholder="From"/>
-  <label htmlFor="floatingInput1">From</label>
-                                     </div>
-                                    <div className="form-floating">
-  <input type="email" className="form-control" id="floatingInput" placeholder="To"/>
-  <label htmlFor="">To</label>
-                                     </div>
+                                        <div className="form-floating ">
+                                            <input type="email" className="form-control" id="floatingInput1" placeholder="From" />
+                                            <label htmlFor="floatingInput1">From</label>
+                                        </div>
+                                        <div className="form-floating">
+                                            <input type="email" className="form-control" id="floatingInput" placeholder="To" />
+                                            <label htmlFor="">To</label>
+                                        </div>
                                     </div>
 
-                                      {/* Color */}
-                                      <h5 className="sub-title mt-3">
-                                       Colors
+                                    {/* Color */}
+                                    <h5 className="sub-title mt-3">
+                                        Colors
                                     </h5>
                                     <div>
                                         <div className="d-flex flex-wrap">
-                                             <ul className="colors ps-0">
+                                            <ul className="colors ps-0">
                                                 <li></li>
                                                 <li></li>
                                                 <li></li>
@@ -87,7 +94,7 @@ const Store = () => {
                                                 <li></li>
                                                 <li></li>
                                                 <li></li>
-                                             </ul>
+                                            </ul>
                                         </div>
                                     </div>
 
@@ -96,19 +103,19 @@ const Store = () => {
                                         Size
                                     </h5>
                                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="color-1" />
-                     <label className="form-check-label" htmlFor="color-1">
-                                              S(2)
-                                            </label>
-                                        </div>
+                                        <input className="form-check-input" type="checkbox" value="" id="color-1" />
+                                        <label className="form-check-label" htmlFor="color-1">
+                                            S(2)
+                                        </label>
+                                    </div>
                                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="color-2" />
-                     <label className="form-check-label" htmlFor="color-2">
-                                              M(2)
-                                            </label>
-                                        </div>
-                                    
-                                    
+                                        <input className="form-check-input" type="checkbox" value="" id="color-2" />
+                                        <label className="form-check-label" htmlFor="color-2">
+                                            M(2)
+                                        </label>
+                                    </div>
+
+
                                 </div>
                             </div>
 
@@ -119,10 +126,10 @@ const Store = () => {
                                 </h3>
                                 <div>
                                     <div className="product-tags d-flex flex-wrap align-items-center gap-10">
-                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Headphone</span>
-                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Laptop</span>
-                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Mobile</span>
-                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Wire</span>
+                                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Headphone</span>
+                                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Laptop</span>
+                                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Mobile</span>
+                                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Wire</span>
                                     </div>
                                 </div>
                             </div>
@@ -138,8 +145,8 @@ const Store = () => {
                                         <h5>
                                             Kid's Watch,it is very smart for all ages.
                                         </h5>
-                                  <ReactStars count={5} size={24} value={4} edit={false} activeColor="#ffd700"/>
-                                   <b>$300</b>
+                                        <ReactStars count={5} size={24} value={4} edit={false} activeColor="#ffd700" />
+                                        <b>$300</b>
                                     </div>
                                 </div>
                                 <div className="random-products d-flex">
@@ -150,52 +157,67 @@ const Store = () => {
                                         <h5>
                                             Kid's Watch,it is very smart for all ages.
                                         </h5>
-                                  <ReactStars count={5} size={24} value={4} edit={false} activeColor="#ffd700"/>
-                                   <b>$300</b>
+                                        <ReactStars count={5} size={24} value={4} edit={false} activeColor="#ffd700" />
+                                        <b>$300</b>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-9">
-     <div className="filter-sort-grid">
-        <div className="d-flex justify-content-between align-items-center">
-        <div className="d-flex align-items-center gap-10">
- <p className="mb-0" style={{"width":"100px"}}>Sort By:</p>
- <select name="" className="form-control form-select" id="">
-    <option value="manual">Featured</option>
-    <option value="best-selling" selected="selected">
-        Best Selling
-    </option>
-    <option value="title-ascending">
-        Alphabetically, Z-A
-    </option>
-    <option value="price-ascending">
-        Price, low to high
-    </option>
-    <option value="price-descending">
-        Price, high to low
-    </option>
-    <option value="created-ascending">
-       Date, old to new
-    </option>
-    <option value="price-descending">
-        Date, new to old
-    </option>
- </select>
-        </div>
+       <div className="col-9">
+  <div className="filter-sort-grid mb-4">
+         <div className="d-flex justify-content-between align-items-center">
+    <div className="d-flex align-items-center gap-10">
+       <p className="mb-0" style={{ "width": "100px" }}>Sort By:</p>
+            <select name="" className="form-control form-select" id="">
+           <option value="manual">Featured</option>
+              <option value="best-selling" selected="selected">
+                                                Best Selling
+          </option>
+   <option value="title-ascending">
+                                                Alphabetically, Z-A
+   </option>
+        <option value="price-ascending">
+                                                Price, low to high
+                  </option>
+          <option value="price-descending">
+                                                Price, high to low
+              </option>
+          <option value="created-ascending">
+                                                Date, old to new
+      </option>
+              <option value="price-descending">
+                                                Date, new to old
+  </option>
+                </select>
+    </div>
+ <div className="d-flex align-items-center gap-10">
+                     <p className="totalProducts mb-0 ">21 Products</p>
+             <div className="d-flex gap-10 align-items-center fs-4">
+                       <FaAlignJustify  className="icon" />
+                  <FaBars  className="icon" />
+          <FaEquals  className="icon" />
+   <FaBars className="icon" />
+                                        </div>
+                                    </div>
 
-<div className="d-flex align-items-center gap-10">
-  <p className="totalProducts mb-0 ">21 Products</p>
-  <div className="d-flex gap-10 align-items-center fs-4">
-  <FaAlignJustify className="icon"/>
-  <FaBars className="icon"/>
-  <FaEquals className="icon"/>
-  <FaBars className="icon"/>
-  </div>
-</div>
+                                </div>
+                            </div>
 
-        </div>
-     </div>
+                            {/* product list */}
+          <div className="products-list pb-5 ">
+              <div className="d-flex flex-wrap  gap-60">
+                <ProductCard  />
+              <ProductCard />
+         <ProductCard />
+        <ProductCard />
+      <ProductCard />
+
+                                    
+                       </div>
+
+
+                 </div>
+
                         </div>
                     </div>
                 </div>
